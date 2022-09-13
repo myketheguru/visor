335f0554347f40efbcdd39e030df9b55df304e92
myketheguru <darkmyketheguru@gmail.com>
2022-09-13 11:19:43 +0100

    Refocused ws declaration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import socketIOClient from 'socket.io-client'
 import type { AppProps } from 'next/app'
-import { ws } from '../room-context/RoomContext'
+// import { ws } from '../room-context/RoomContext'
+
+const WS = 'http://localhost:8080'
+export const ws = socketIOClient(WS)
 
 
 function MyApp({ Component, pageProps }: AppProps) {
